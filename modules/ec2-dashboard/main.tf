863412b4c7942a6025b2f327303ee853d19fe069
Clay Smith <clay.smith@servicenow.com>
2022-02-10 10:59:56 -0800

    bump version to v1.60.0

--- modules/ec2-dashboard/main.tf
+++ modules/ec2-dashboard/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     lightstep = {
       source  = "lightstep/lightstep"
-      version = "1.51.6"
+      version = "1.60.0"
     }
   }
   required_version = ">= v1.0.11"
