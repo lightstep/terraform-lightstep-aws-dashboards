0d616f644343b810992ec1339f430ac531efd629
Heber Silva <heber.silva@lightstep.com>
2022-11-09 17:16:15 -0500

    Dashboard/aws natgateway (#33)
    * /feature adding aws Terraform main.tf, variable.tf, outputs.tf for natgateway
    
    * Added aws natgateway terraform lightstep dashboard
    
    * update capitalization
    
    Changes to capitalization have been captured in code, but it would be inefficient to regenerate this fileset at this time.
    
    * update caps
    
    Co-authored-by: Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -67,3 +67,7 @@ output "lightstep_aurora_dashboard_url" {
   value       = module.lightstep_aurora_dashboard.dashboard_url
   description = "Lightstep AWS Aurora Dashboard URL"
 }
+output "lightstep_natgateway_dashboard_url" {
+  value       = module.lightstep_natgateway_dashboard.dashboard_url
+  description = "Lightstep AWS NATGateway Dashboard URL"
+}
