21b942c691f3e5a9498f837585ac85c98529cdfe
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2022-11-28 17:59:36 -0600

    Update CloudFront dashboard

--- modules/cloudfront-dashboard/main.tf
+++ modules/cloudfront-dashboard/main.tf
@@ -2,115 +2,93 @@ terraform {
   required_providers {
     lightstep = {
       source  = "lightstep/lightstep"
-      version = "~> 1.60.2"
+      version = "~> 1.70.9"
     }
   }
   required_version = ">= v1.0.11"
 }
 
-resource "lightstep_metric_dashboard" "aws_cloudfront_dashboard" {
+resource "lightstep_dashboard" "aws_cloudfront_dashboard" {
   project_name   = var.lightstep_project
   dashboard_name = "AWS CloudFront"
 
   chart {
-    name = "Requests"
+    name = "Requests / TotalErrorRate"
     rank = "0"
     type = "timeseries"
 
     query {
-      query_name = "a"
-      display    = "bar"
-      hidden     = false
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = "metric aws.cloudfront.requests_sum | delta | group_by [], sum"
+    }
 
-      metric              = "aws.cloudfront.requests_count"
-      timeseries_operator = "delta"
-
-
-      group_by {
-        aggregation_method = "sum"
-        keys               = ["DistributionId", ]
-      }
-
+    query {
+      query_name   = "b"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+with
+  a = metric aws.cloudfront.total_error_rate_sum | reduce sum | group_by [], sum;
+  b = metric aws.cloudfront.total_error_rate_count | reduce sum | group_by [], sum;
+join (a / b), a = 0, b = 0
+EOT
     }
 
   }
 
   chart {
-    name = "4xx Error Rate"
+    name = "4xxErrorRate / 5xxErrorRate"
     rank = "1"
     type = "timeseries"
 
     query {
-      query_name = "a"
-      display    = "bar"
-      hidden     = false
-
-      metric              = "aws.cloudfront.4_xx_error_rate_count"
-      timeseries_operator = "delta"
-
-
-      group_by {
-        aggregation_method = "sum"
-        keys               = ["DistributionId", ]
-      }
-
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+with
+  a = metric aws.cloudfront.4xx_error_rate_sum | reduce sum | group_by [], sum;
+  b = metric aws.cloudfront.4xx_error_rate_count | reduce sum | group_by [], sum;
+join (a / b), a = 0, b = 0
+EOT
     }
 
     query {
-      query_name = "b"
-      display    = "bar"
-      hidden     = false
-
-      metric              = "aws.cloudfront.5xx_error_rate_count"
-      timeseries_operator = "delta"
-
-
-      group_by {
-        aggregation_method = "sum"
-        keys               = ["DistributionId", ]
-      }
-
+      query_name   = "b"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+with
+  a = metric aws.cloudfront.5xx_error_rate_sum | reduce sum | group_by [], sum;
+  b = metric aws.cloudfront.5xx_error_rate_count | reduce sum | group_by [], sum;
+join (a / b), a = 0, b = 0
+EOT
     }
 
   }
 
   chart {
-    name = "Bytes Downloaded"
+    name = "BytesDownloaded / BytesUploaded"
     rank = "2"
     type = "timeseries"
 
     query {
-      query_name = "a"
-      display    = "bar"
-      hidden     = false
-
-      metric              = "aws.cloudfront.bytes_downloaded_count"
-      timeseries_operator = "delta"
-
-
-      group_by {
-        aggregation_method = "sum"
-        keys               = ["DistributionId", ]
-      }
-
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = "metric aws.cloudfront.bytes_downloaded_sum | delta | group_by [], sum"
     }
 
     query {
-      query_name = "b"
-      display    = "bar"
-      hidden     = false
-
-      metric              = "aws.cloudfront.bytes_uploaded_count"
-      timeseries_operator = "delta"
-
-
-      group_by {
-        aggregation_method = "sum"
-        keys               = ["DistributionId", ]
-      }
-
+      query_name   = "b"
+      display      = "line"
+      hidden       = false
+      query_string = "metric aws.cloudfront.bytes_uploaded_sum | delta | group_by [], sum"
     }
 
   }
 
 }
+
