6ab3c70879fea0f8a377048c48f6180d0692b411
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2022-12-05 17:06:23 -0600

    Add SES

---
+++ modules/ses-dashboard/main.tf
@@ -0,0 +1,96 @@
+terraform {
+  required_providers {
+    lightstep = {
+      source  = "lightstep/lightstep"
+      version = "~> 1.70.9"
+    }
+  }
+  required_version = ">= v1.0.11"
+}
+
+resource "lightstep_dashboard" "aws_ses_dashboard" {
+  project_name   = var.lightstep_project
+  dashboard_name = "AWS SES"
+
+  chart {
+    name = "Send / Delivery"
+    rank = "0"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+with
+  a = metric aws.ses.send_sum | delta | group_by [], sum;
+  b = metric aws.ses.send_count | delta | group_by [], sum;
+join (a / b), a = 0, b = 0
+EOT
+    }
+
+    query {
+      query_name   = "b"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+with
+  a = metric aws.ses.delivery_sum | delta | group_by [], sum;
+  b = metric aws.ses.delivery_count | delta | group_by [], sum;
+join (a / b), a = 0, b = 0
+EOT
+    }
+
+  }
+
+  chart {
+    name = "Bounce / Reject"
+    rank = "1"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+with
+  a = metric aws.ses.bounce_sum | delta | group_by [], sum;
+  b = metric aws.ses.bounce_count | delta | group_by [], sum;
+join (a / b), a = 0, b = 0
+EOT
+    }
+
+    query {
+      query_name   = "b"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+with
+  a = metric aws.ses.reject_sum | delta | group_by [], sum;
+  b = metric aws.ses.reject_count | delta | group_by [], sum;
+join (a / b), a = 0, b = 0
+EOT
+    }
+
+  }
+
+  chart {
+    name = "Complaint"
+    rank = "2"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+with
+  a = metric aws.ses.complaint_sum | delta | group_by [], sum;
+  b = metric aws.ses.complaint_count | delta | group_by [], sum;
+join (a / b), a = 0, b = 0
+EOT
+    }
+
+  }
+
+}
