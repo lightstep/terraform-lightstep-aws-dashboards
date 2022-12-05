6e7a34f56d82158efd62da147abf21977f6d7ea1
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2022-12-05 16:36:06 -0600

    Add Certificate Manager

---
+++ modules/certificatemanager-dashboard/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    lightstep = {
+      source  = "lightstep/lightstep"
+      version = "~> 1.70.9"
+    }
+  }
+  required_version = ">= v1.0.11"
+}
+
+resource "lightstep_dashboard" "aws_certificatemanager_dashboard" {
+  project_name   = var.lightstep_project
+  dashboard_name = "AWS Certificate Manager"
+
+  chart {
+    name = "Days To Expiry"
+    rank = "0"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+with
+  a = metric aws.certificatemanager.days_to_expiry_sum | delta | group_by [], sum;
+  b = metric aws.certificatemanager.days_to_expiry_count | delta | group_by [], sum;
+join (a / b), a = 0, b = 0
+EOT
+    }
+
+  }
+
+}
