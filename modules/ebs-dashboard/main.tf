42f07478f6f70af41455abe68066cd9826671d4e
Greg Dallavalle <greg.dallavalle@lightstep.com>
2022-12-13 11:30:15 -0600

    relax version pins to ~>1.60 or ~>1.70

--- modules/ebs-dashboard/main.tf
+++ modules/ebs-dashboard/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     lightstep = {
       source  = "lightstep/lightstep"
-      version = "~> 1.70.9"
+      version = "~> 1.70"
     }
   }
   required_version = ">= v1.0.11"
