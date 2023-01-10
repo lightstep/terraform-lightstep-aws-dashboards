61d5f767f226056bb10bf3d01c22b2ff3bd251fa
Ryan Vacek <ryanvacek@Ryans-MacBook-Pro.local>
2023-01-10 10:47:25 -0700

    fixes camel case

--- modules/msk-topic-dashboard/outputs.tf
+++ modules/msk-topic-dashboard/outputs.tf
@@ -1,4 +1,4 @@
 output "dashboard_url" {
-  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.aws_mks-topic_dashboard.id}"
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.aws_mks_topic_dashboard.id}"
   description = "MSK Topic Dashboard URL"
 }
