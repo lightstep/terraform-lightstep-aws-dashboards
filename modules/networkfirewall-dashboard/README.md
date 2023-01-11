b7d00aa8eaa3ef6c659a092a5e494ad6637da40b
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-01-11 15:40:42 -0600

    Add Network Firewall (#54)

---
+++ modules/networkfirewall-dashboard/README.md
@@ -0,0 +1,3 @@
+# Lightstep AWS Network Firewall Dashboard
+
+This module provisions a Lightstep dashboard for AWS Network Firewall.
---
+++ modules/networkfirewall-dashboard/main.tf
@@ -0,0 +1,96 @@
+terraform {
+  required_providers {
+    lightstep = {
+      source  = "lightstep/lightstep"
+      version = "~> 1.70.9"
+    }
+  }
+  required_version = ">= v1.0.11"
+}
+
+resource "lightstep_dashboard" "aws_networkfirewall_dashboard" {
+  project_name   = var.lightstep_project
+  dashboard_name = "AWS Network Firewall"
+
+  chart {
+    name = "Passed Packets / Dropped Packets "
+    rank = "0"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+with
+  a = metric aws.networkfirewall.passed_packets_sum | delta | group_by [], sum;
+  b = metric aws.networkfirewall.passed_packets_count | delta | group_by [], sum;
+join (a / b), a = 0, b = 0
+EOT
+    }
+
+    query {
+      query_name   = "b"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+with
+  a = metric aws.networkfirewall.dropped_packets_sum | delta | group_by [], sum;
+  b = metric aws.networkfirewall.dropped_packets_count | delta | group_by [], sum;
+join (a / b), a = 0, b = 0
+EOT
+    }
+
+  }
+
+  chart {
+    name = "Received Packets Count"
+    rank = "1"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+with
+  a = metric aws.networkfirewall.received_packet_count_sum | delta | group_by [], sum;
+  b = metric aws.networkfirewall.received_packet_count_count | delta | group_by [], sum;
+join (a / b), a = 0, b = 0
+EOT
+    }
+
+  }
+
+  chart {
+    name = "Packets / Bytes Out"
+    rank = "2"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+with
+  a = metric aws.networkfirewall.packets_sum | delta | group_by [], sum;
+  b = metric aws.networkfirewall.packets_count | delta | group_by [], sum;
+join (a / b), a = 0, b = 0
+EOT
+    }
+
+    query {
+      query_name   = "b"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+with
+  a = metric aws.transitgateway.bytes_out_sum | delta | group_by [], sum;
+  b = metric aws.transitgateway.bytes_out_count | delta | group_by [], sum;
+join (a / b), a = 0, b = 0
+EOT
+    }
+
+  }
+
+}
