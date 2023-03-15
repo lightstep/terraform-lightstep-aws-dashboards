6392224958f2125256457b3dbfa420d2f72bd186
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-03-15 16:49:08 -0500

    add header ebs

--- modules/ebs-dashboard/main.tf
+++ modules/ebs-dashboard/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    lightstep = {
+      source  = "lightstep/lightstep"
+      version = "~> 1.70.10"
+    }
+  }
+  required_version = ">= v1.0.11"
+}
+
 resource "lightstep_dashboard" "aws_ebs_dashboard" {
   project_name          = var.lightstep_project
   dashboard_name        = "AWS EBS"
