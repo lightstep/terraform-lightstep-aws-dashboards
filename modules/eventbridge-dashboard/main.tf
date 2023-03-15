4346ccf54e8433897c77899738b2048b22022386
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-03-15 16:28:14 -0500

    manual adjustments eventbridge

--- modules/eventbridge-dashboard/main.tf
+++ modules/eventbridge-dashboard/main.tf
@@ -1,6 +1,6 @@
 
 resource "lightstep_dashboard" "aws_eventbridge_dashboard" {
-  project_name          = var.project
+  project_name          = var.lightstep_project
   dashboard_name        = "AWS EventBridge"
   dashboard_description = "Monitor AWS EventBridge in this overview."
 
@@ -13,7 +13,9 @@ resource "lightstep_dashboard" "aws_eventbridge_dashboard" {
       query_name   = "a"
       display      = "line"
       hidden       = false
-      query_string = "metric aws.events.triggered_rules_sum | delta | group_by [], sum"
+      query_string = <<EOT
+metric aws.events.triggered_rules_sum | delta | group_by [], sum
+EOT
     }
 
   }
@@ -27,7 +29,9 @@ resource "lightstep_dashboard" "aws_eventbridge_dashboard" {
       query_name   = "a"
       display      = "line"
       hidden       = false
-      query_string = "metric aws.events.invocations_sum | delta | group_by [], sum"
+      query_string = <<EOT
+metric aws.events.invocations_sum | delta | group_by [], sum
+EOT
     }
 
   }
@@ -41,35 +45,45 @@ resource "lightstep_dashboard" "aws_eventbridge_dashboard" {
       query_name   = "a"
       display      = "line"
       hidden       = false
-      query_string = "metric aws.events.dead_letter_invocations_sum | delta | group_by [], sum"
+      query_string = <<EOT
+metric aws.events.dead_letter_invocations_sum | delta | group_by [], sum
+EOT
     }
 
     query {
       query_name   = "b"
       display      = "line"
       hidden       = false
-      query_string = "metric aws.events.failed_invocations_sum | delta | group_by [], sum"
+      query_string = <<EOT
+metric aws.events.failed_invocations_sum | delta | group_by [], sum
+EOT
     }
 
     query {
       query_name   = "c"
       display      = "line"
       hidden       = false
-      query_string = "metric aws.events.invocations_failed_to_be_sent_to_dlq_sum | delta | group_by [], sum"
+      query_string = <<EOT
+metric aws.events.invocations_failed_to_be_sent_to_dlq_sum | delta | group_by [], sum
+EOT
     }
 
     query {
       query_name   = "d"
       display      = "line"
       hidden       = false
-      query_string = "metric aws.events.invocations_sent_to_dlq_sum | delta | group_by [], sum"
+      query_string = <<EOT
+metric aws.events.invocations_sent_to_dlq_sum | delta | group_by [], sum
+EOT
     }
 
     query {
       query_name   = "e"
       display      = "line"
       hidden       = false
-      query_string = "metric aws.events.throttled_rules_sum | delta | group_by [], sum"
+      query_string = <<EOT
+metric aws.events.throttled_rules_sum | delta | group_by [], sum
+EOT
     }
   }
 
@@ -82,7 +96,9 @@ resource "lightstep_dashboard" "aws_eventbridge_dashboard" {
       query_name   = "a"
       display      = "line"
       hidden       = false
-      query_string = "metric aws.events.matched_events_sum | delta | group_by [], sum"
+      query_string = <<EOT
+metric aws.events.matched_events_sum | delta | group_by [], sum
+EOT
     }
   }
 
@@ -95,7 +111,9 @@ resource "lightstep_dashboard" "aws_eventbridge_dashboard" {
       query_name   = "a"
       display      = "line"
       hidden       = false
-      query_string = "metric aws.events.ingestion_to_invocation_start_latency_sum | delta | group_by [], sum"
+      query_string = <<EOT
+metric aws.events.ingestion_to_invocation_start_latency_sum | delta | group_by [], sum
+EOT
     }
   }
 }
