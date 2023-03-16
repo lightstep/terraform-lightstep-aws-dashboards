ea5dd3d25b8009b1ee16497af7c886406ff4a7fb
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-03-16 16:32:10 -0500

    [amplify] reformat for prebuilt

--- modules/amplify-dashboard/main.tf
+++ modules/amplify-dashboard/main.tf
@@ -8,9 +8,10 @@ terraform {
   required_version = ">= v1.0.11"
 }
 
-resource "lightstep_metric_dashboard" "aws_amplify_dashboard" {
-  project_name   = var.lightstep_project
-  dashboard_name = "AWS Amplify"
+resource "lightstep_dashboard" "aws_amplify_dashboard" {
+  project_name          = var.lightstep_project
+  dashboard_name        = "AWS Amplify"
+  dashboard_description = ""
 
   chart {
     name = "Requests Count"
@@ -18,19 +19,10 @@ resource "lightstep_metric_dashboard" "aws_amplify_dashboard" {
     type = "timeseries"
 
     query {
-      query_name = "a"
-      display    = "bar"
-      hidden     = false
-
-      metric              = "aws.amplify.requests_sum"
-      timeseries_operator = "delta"
-
-
-      group_by {
-        aggregation_method = "sum"
-        keys               = []
-      }
-
+      query_name   = "a"
+      display      = "bar"
+      hidden       = false
+      query_string = "metric aws.amplify.requests_sum | delta | group_by [], sum"
     }
 
   }
@@ -41,35 +33,17 @@ resource "lightstep_metric_dashboard" "aws_amplify_dashboard" {
     type = "timeseries"
 
     query {
-      query_name = "a"
-      display    = "bar"
-      hidden     = false
-
-      metric              = "aws.amplify.bytes_downloaded_sum"
-      timeseries_operator = "delta"
-
-
-      group_by {
-        aggregation_method = "sum"
-        keys               = []
-      }
-
+      query_name   = "a"
+      display      = "bar"
+      hidden       = false
+      query_string = "metric aws.amplify.bytes_downloaded_sum | delta | group_by [], sum"
     }
 
     query {
-      query_name = "b"
-      display    = "bar"
-      hidden     = false
-
-      metric              = "aws.amplify.bytes_uploaded_sum"
-      timeseries_operator = "delta"
-
-
-      group_by {
-        aggregation_method = "sum"
-        keys               = []
-      }
-
+      query_name   = "b"
+      display      = "bar"
+      hidden       = false
+      query_string = "metric aws.amplify.bytes_uploaded_sum | delta | group_by [], sum"
     }
 
   }
@@ -80,35 +54,17 @@ resource "lightstep_metric_dashboard" "aws_amplify_dashboard" {
     type = "timeseries"
 
     query {
-      query_name = "a"
-      display    = "bar"
-      hidden     = false
-
-      metric              = "aws.amplify.4xx_errors_rate_sum"
-      timeseries_operator = "delta"
-
-
-      group_by {
-        aggregation_method = "sum"
-        keys               = []
-      }
-
+      query_name   = "a"
+      display      = "bar"
+      hidden       = false
+      query_string = "metric aws.amplify.4xx_errors_rate_sum | delta | group_by [], sum"
     }
 
     query {
-      query_name = "b"
-      display    = "bar"
-      hidden     = false
-
-      metric              = "aws.amplify.5xx_errors_rate_sum"
-      timeseries_operator = "delta"
-
-
-      group_by {
-        aggregation_method = "sum"
-        keys               = []
-      }
-
+      query_name   = "b"
+      display      = "bar"
+      hidden       = false
+      query_string = "metric aws.amplify.5xx_errors_rate_sum | delta | group_by [], sum"
     }
 
   }
@@ -119,19 +75,10 @@ resource "lightstep_metric_dashboard" "aws_amplify_dashboard" {
     type = "timeseries"
 
     query {
-      query_name = "a"
-      display    = "bar"
-      hidden     = false
-
-      metric              = "aws.amplify.latency_max"
-      timeseries_operator = "last"
-
-
-      group_by {
-        aggregation_method = "sum"
-        keys               = []
-      }
-
+      query_name   = "a"
+      display      = "bar"
+      hidden       = false
+      query_string = "metric aws.amplify.latency_max | latest | group_by [], sum"
     }
 
   }
