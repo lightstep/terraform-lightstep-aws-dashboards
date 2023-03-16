b75ea2ef8121588835f2b3d2bd59659f3123b2e3
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-03-16 17:44:41 -0500

    Fix to lightstep_dashboard (#72)

--- modules/efs-dashboard/main.tf
+++ modules/efs-dashboard/main.tf
@@ -8,7 +8,7 @@ terraform {
   required_version = ">= v1.0.11"
 }
 
-resource "lightstep_metric_dashboard" "aws_sns_dashboard" {
+resource "lightstep_metric_dashboard" "aws_efs_dashboard" {
   project_name   = var.lightstep_project
   dashboard_name = "AWS EFS"
 
--- modules/efs-dashboard/outputs.tf
+++ modules/efs-dashboard/outputs.tf
@@ -1,4 +1,4 @@
 output "dashboard_url" {
-  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_metric_dashboard.aws_sns_dashboard.id}"
-  description = "SNS Dashboard URL"
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.aws_efs_dashboard.id}"
+  description = "EFS Dashboard URL"
 }
