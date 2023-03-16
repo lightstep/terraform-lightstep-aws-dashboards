105433959c4347f6bcf9b338a135c87756770560
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-03-16 17:19:31 -0500

    [natgateway] fmt

--- modules/natgateway-dashboard/main.tf
+++ modules/natgateway-dashboard/main.tf
@@ -10,8 +10,8 @@ terraform {
 
 
 resource "lightstep_dashboard" "aws_natgateway_dashboard" {
-  project_name   = var.lightstep_project
-  dashboard_name = "AWS NAT Gateway"
+  project_name          = var.lightstep_project
+  dashboard_name        = "AWS NAT Gateway"
   dashboard_description = "Monitor AWS NAT Gateway with this summary dashboard."
 
   chart {
