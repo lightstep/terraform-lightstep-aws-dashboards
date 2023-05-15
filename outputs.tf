be7ada897467baa7b4c344d716d83fb5d96e721f
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-05-15 16:24:29 -0500

    Update root module (#90)

--- outputs.tf
+++ outputs.tf
@@ -1,135 +1,195 @@
-output "lightstep_ec2_dashboard_url" {
-  value       = module.lightstep_ec2_dashboard.dashboard_url
-  description = "Lightstep AWS EC2 Dashboard URL"
-}
 
-output "lightstep_rds_dashboard_url" {
-  value       = module.lightstep_rds_dashboard.dashboard_url
-  description = "Lightstep AWS RDS Dashboard URL"
-}
-
-output "lightstep_app_elb_dashboard_url" {
-  value       = module.lightstep_app_elb_dashboard.dashboard_url
-  description = "Lightstep AWS Application ELB Dashboard URL"
-}
-
-output "lightstep_lambda_dashboard_url" {
-  value       = module.lightstep_lambda_dashboard.dashboard_url
-  description = "Lightstep AWS Lambda Dashboard URL"
-}
-
-output "lightstep_kinesis_dashboard_url" {
-  value       = module.lightstep_kinesis_dashboard.dashboard_url
-  description = "Lightstep AWS Kinesis Dashboard URL"
-}
-
-output "lightstep_sqs_dashboard_url" {
-  value       = module.lightstep_sqs_dashboard.dashboard_url
-  description = "Lightstep AWS SQS Dashboard URL"
-}
-
-output "lightstep_sns_dashboard_url" {
-  value       = module.lightstep_sns_dashboard.dashboard_url
-  description = "Lightstep AWS SNS Dashboard URL"
-}
-
-output "lightstep_dynamodb_dashboard_url" {
-  value       = module.lightstep_dynamodb_dashboard.dashboard_url
-  description = "Lightstep AWS DyanamoDB Dashboard URL"
+output "lightstep_amplify_dashboard_url" {
+  value       = module.lightstep_amplify_dashboard.dashboard_url
+  description = "Lightstep AWS Amplify Dashboard"
 }
 
 output "lightstep_apigateway_dashboard_url" {
   value       = module.lightstep_apigateway_dashboard.dashboard_url
-  description = "Lightstep AWS API Gateway Dashboard URL"
+  description = "Lightstep AWS API Gateway Dashboard"
 }
 
-output "lightstep_ecs_dashboard_url" {
-  value       = module.lightstep_ecs_dashboard.dashboard_url
-  description = "Lightstep AWS ECS Dashboard URL"
+output "lightstep_applicationelb_dashboard_url" {
+  value       = module.lightstep_applicationelb_dashboard.dashboard_url
+  description = "Lightstep AWS Application ELB Load Balancer Dashboard"
 }
 
-output "lightstep_elasticache_redis_dashboard_url" {
-  value       = module.lightstep_elasticache_redis_dashboard.dashboard_url
-  description = "Lightstep AWS Elasticache (Redis) Dashboard URL"
-}
-
-output "lightstep_s3_dashboard_url" {
-  value       = module.lightstep_s3_dashboard.dashboard_url
-  description = "Lightstep AWS S3 Dashboard URL"
-}
-
-output "lightstep_efs_dashboard_url" {
-  value       = module.lightstep_efs_dashboard.dashboard_url
-  description = "Lightstep AWS EFS Dashboard URL"
+output "lightstep_athena_dashboard_url" {
+  value       = module.lightstep_athena_dashboard.dashboard_url
+  description = "Lightstep AWS  Dashboard"
 }
 
 output "lightstep_aurora_dashboard_url" {
   value       = module.lightstep_aurora_dashboard.dashboard_url
-  description = "Lightstep AWS Aurora Dashboard URL"
+  description = "Lightstep AWS Aurora Dashboard"
 }
 
-output "lightstep_msk_dashboard_url" {
-  value       = module.lightstep_msk_dashboard.dashboard_url
-  description = "Lightstep AWS MSK Dashboard URL"
-}
-
-
-output "lightstep_route53_dashboard_url" {
-  value       = module.lightstep_route53_dashboard.dashboard_url
-  description = "Lightstep AWS Route 53 Dashboard URL"
-}
-
-output "lightstep_cloudfront_dashboard_url" {
-  value       = module.lightstep_cloudfront_dashboard.dashboard_url
-  description = "Lightstep AWS CloudFront Dashboard URL"
-}
-
-output "lightstep_inspector_dashboard_url" {
-  value       = module.lightstep_inspector_dashboard.dashboard_url
-  description = "Lightstep AWS Inspector Dashboard URL"
-}
-
-output "lightstep_natgateway_dashboard_url" {
-  value       = module.lightstep_natgateway_dashboard.dashboard_url
-  description = "Lightstep AWS NATGateway Dashboard URL"
-}
-
-output "lightstep_amplify_dashboard_url" {
-  value       = module.lightstep_amplify_dashboard.dashboard_url
-  description = "Lightstep AWS Amplify Dashboard URL"
-}
-
-output "lightstep_cloudtrail_dashboard_url" {
-  value       = module.lightstep_cloudtrail_dashboard.dashboard_url
-  description = "Lightstep AWS CloudTrail Dashboard URL"
+output "lightstep_autoscaling_dashboard_url" {
+  value       = module.lightstep_autoscaling_dashboard.dashboard_url
+  description = "Lightstep AWS Auto Scaling Dashboard"
 }
 
 output "lightstep_backup_dashboard_url" {
-  value       = module.lightstep_nbackup_dashboard.dashboard_url
-  description = "Lightstep AWS Backup Dashboard URL"
+  value       = module.lightstep_backup_dashboard.dashboard_url
+  description = "Lightstep AWS Backup Dashboard"
+}
+
+output "lightstep_certificatemanager_dashboard_url" {
+  value       = module.lightstep_certificatemanager_dashboard.dashboard_url
+  description = "Lightstep AWS Certificate Manager Dashboard"
 }
 
 output "lightstep_chatbot_dashboard_url" {
   value       = module.lightstep_chatbot_dashboard.dashboard_url
-  description = "Lightstep AWS Chatbot Dashboard URL"
-}
-
-output "lightstep_cloudhsm_dashboard_url" {
-  value       = module.lightstep_cloudhsm_dashboard.dashboard_url
-  description = "Lightstep AWS CloudHSM Dashboard URL"
+  description = "Lightstep AWS Chatbot Dashboard"
 }
 
 output "lightstep_chime_dashboard_url" {
   value       = module.lightstep_chime_dashboard.dashboard_url
-  description = "Lightstep AWS Chime Dashboard URL"
+  description = "Lightstep AWS Chime SDK Dashboard"
+}
+
+output "lightstep_cloudfront_dashboard_url" {
+  value       = module.lightstep_cloudfront_dashboard.dashboard_url
+  description = "Lightstep AWS CloudFront Dashboard"
+}
+
+output "lightstep_cloudhsm_dashboard_url" {
+  value       = module.lightstep_cloudhsm_dashboard.dashboard_url
+  description = "Lightstep AWS CloudHSM Dashboard"
+}
+
+output "lightstep_cloudtrail_dashboard_url" {
+  value       = module.lightstep_cloudtrail_dashboard.dashboard_url
+  description = "Lightstep AWS CloudTrail Dashboard"
+}
+
+output "lightstep_cognito_dashboard_url" {
+  value       = module.lightstep_cognito_dashboard.dashboard_url
+  description = "Lightstep AWS COGNITO Dashboard"
+}
+
+output "lightstep_dynamodb_dashboard_url" {
+  value       = module.lightstep_dynamodb_dashboard.dashboard_url
+  description = "Lightstep AWS DynamoDB Dashboard"
+}
+
+output "lightstep_ebs_dashboard_url" {
+  value       = module.lightstep_ebs_dashboard.dashboard_url
+  description = "Lightstep AWS EBS Dashboard"
+}
+
+output "lightstep_ec2_dashboard_url" {
+  value       = module.lightstep_ec2_dashboard.dashboard_url
+  description = "Lightstep AWS EC2 Dashboard"
+}
+
+output "lightstep_ecs_dashboard_url" {
+  value       = module.lightstep_ecs_dashboard.dashboard_url
+  description = "Lightstep AWS ECS Dashboard"
+}
+
+output "lightstep_efs_dashboard_url" {
+  value       = module.lightstep_efs_dashboard.dashboard_url
+  description = "Lightstep AWS EFS Dashboard"
 }
 
 output "lightstep_eks_node_dashboard_url" {
   value       = module.lightstep_eks_node_dashboard.dashboard_url
-  description = "Lightstep AWS EKS Node Dashboard URL"
+  description = "Lightstep AWS EKS Node Dashboard"
 }
 
 output "lightstep_eks_pod_dashboard_url" {
   value       = module.lightstep_eks_pod_dashboard.dashboard_url
-  description = "Lightstep AWS EKS Pods Dashboard URL"
+  description = "Lightstep AWS EKS Pod Dashboard"
+}
+
+output "lightstep_elasticache_redis_dashboard_url" {
+  value       = module.lightstep_elasticache_redis_dashboard.dashboard_url
+  description = "Lightstep AWS ElastiCache (Redis) Dashboard"
+}
+
+output "lightstep_elasticmapreduce_dashboard_url" {
+  value       = module.lightstep_elasticmapreduce_dashboard.dashboard_url
+  description = "Lightstep AWS EMR Hadoop 2 Dashboard"
+}
+
+output "lightstep_eventbridge_dashboard_url" {
+  value       = module.lightstep_eventbridge_dashboard.dashboard_url
+  description = "Lightstep AWS EventBridge Dashboard"
+}
+
+output "lightstep_inspector_dashboard_url" {
+  value       = module.lightstep_inspector_dashboard.dashboard_url
+  description = "Lightstep AWS Inspector Dashboard"
+}
+
+output "lightstep_kinesis_dashboard_url" {
+  value       = module.lightstep_kinesis_dashboard.dashboard_url
+  description = "Lightstep AWS Kinesis Dashboard"
+}
+
+output "lightstep_lambda_dashboard_url" {
+  value       = module.lightstep_lambda_dashboard.dashboard_url
+  description = "Lightstep AWS Lambda Dashboard"
+}
+
+output "lightstep_msk_dashboard_url" {
+  value       = module.lightstep_msk_dashboard.dashboard_url
+  description = "Lightstep AWS MSK Dashboard"
+}
+
+output "lightstep_msk_topic_dashboard_url" {
+  value       = module.lightstep_msk_topic_dashboard.dashboard_url
+  description = "Lightstep AWS Kafka Topic Dashboard"
+}
+
+output "lightstep_natgateway_dashboard_url" {
+  value       = module.lightstep_natgateway_dashboard.dashboard_url
+  description = "Lightstep AWS NAT Gateway Dashboard"
+}
+
+output "lightstep_networkfirewall_dashboard_url" {
+  value       = module.lightstep_networkfirewall_dashboard.dashboard_url
+  description = "Lightstep AWS Network Firewall Dashboard"
+}
+
+output "lightstep_rds_dashboard_url" {
+  value       = module.lightstep_rds_dashboard.dashboard_url
+  description = "Lightstep AWS RDS Dashboard"
+}
+
+output "lightstep_redshift_dashboard_url" {
+  value       = module.lightstep_redshift_dashboard.dashboard_url
+  description = "Lightstep AWS Redshift Dashboard"
+}
+
+output "lightstep_route53_dashboard_url" {
+  value       = module.lightstep_route53_dashboard.dashboard_url
+  description = "Lightstep AWS Route53 Dashboard"
+}
+
+output "lightstep_s3_dashboard_url" {
+  value       = module.lightstep_s3_dashboard.dashboard_url
+  description = "Lightstep AWS S3 Dashboard"
+}
+
+output "lightstep_ses_dashboard_url" {
+  value       = module.lightstep_ses_dashboard.dashboard_url
+  description = "Lightstep AWS SES Dashboard"
+}
+
+output "lightstep_sns_dashboard_url" {
+  value       = module.lightstep_sns_dashboard.dashboard_url
+  description = "Lightstep AWS SNS Dashboard"
+}
+
+output "lightstep_sqs_dashboard_url" {
+  value       = module.lightstep_sqs_dashboard.dashboard_url
+  description = "Lightstep AWS SQS Dashboard"
+}
+
+output "lightstep_stepfunctions_dashboard_url" {
+  value       = module.lightstep_stepfunctions_dashboard.dashboard_url
+  description = "Lightstep AWS Step Functions Dashboard"
 }
