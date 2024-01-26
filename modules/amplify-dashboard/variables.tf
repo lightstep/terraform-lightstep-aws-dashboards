ad59751a0aafd33b0bff36b6585126afabb03ec8
adil-sultan <87725682+adil-sultan@users.noreply.github.com>
2024-01-25 16:59:31 -0800

    testing the linters and precommit

--- modules/amplify-dashboard/variables.tf
+++ modules/amplify-dashboard/variables.tf
@@ -2,3 +2,8 @@ variable "lightstep_project" {
   description = "Name of Cloud Observability project"
   type        = string
 }
+
+variable "lightstep_test" {
+  description = "Testing Linters"
+  type        = string
+}
