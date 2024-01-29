181751717c4be7b9690482163ecff9e9d300755e
adil-sultan <87725682+adil-sultan@users.noreply.github.com>
2024-01-29 08:45:37 -0800

    Revert "Revert "reverting linter and pre-commit test""
    This reverts commit 78ebce0cf792c9734599ef21fcc39c48d678d15c.

--- modules/amplify-dashboard/variables.tf
+++ modules/amplify-dashboard/variables.tf
@@ -2,8 +2,3 @@ variable "lightstep_project" {
   description = "Name of Cloud Observability project"
   type        = string
 }
-
-variable "lightstep_test" {
-  description = "Testing Linters"
-  type        = string
-}
